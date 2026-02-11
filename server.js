a4675f453e32ebca6a0785ed8734bf4572357411
jord655 <daviddike930@gmail.com>
2026-02-11 01:19:28 +0100

    Fix: remove trailing slash from CORS origin

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
-    'https://aquamarine-khapse-597e4e.netlify.app/'
+    'https://aquamarine-khapse-597e4e.netlify.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
